af84c2fbf9d8e81652ffa0ed691cea7dd69a3ffe
Brendon Stueben <bstueben2001@gmail.com>
2026-05-20 20:56:08 -0600

    added put and delete requests to bounty hunter project

--- level5/bountyHunter/Routes/bountyRouter.js
+++ level5/bountyHunter/Routes/bountyRouter.js
@@ -22,4 +22,24 @@ bountyRouter.post("/", (req, res) => {
   res.send(`Successfully added ${newBounty.firstName} ${newBounty.lastName} to the database! Here's their ID: ${newBounty._id}`);
 });
 
+bountyRouter.put('/:bountyId', (req, res) => {
+    const bountyId = req.params.bountyId
+    const updatedObject = req.body
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+
+    if (bountyIndex !== -1) {
+        const updatedBounty = Object.assign(bounties[bountyIndex], updatedObject)
+        res.send(updatedBounty)
+    } else {
+        res.status(404).send('Bounty not found')
+    }
+})
+
+bountyRouter.delete("/:bountyId", (req, res) => {
+    const bountyId = req.params.bountyId
+    const bountyIndex = bounties.findIndex(bounty => bounty._id === bountyId)
+    bounties.splice(bountyIndex, 1)
+    res.send(bounties)
+})
+
 module.exports = bountyRouter;
--- level5/serverSandbox/routes/movieRouter.js
+++ level5/serverSandbox/routes/movieRouter.js
@@ -4,17 +4,32 @@ const {v4: uuidv4} = require('uuid')
 
 
 const movies = [
-  {title: "Project Hail Mary", year: 2026, _id: uuidv4()},
-  {title: "The Dark Knight", year: 2008, _id: uuidv4()},
-  {title: "Inception", year: 2010, _id: uuidv4()},
-  {title: "Interstellar", year: 2014, _id: uuidv4()},
-  {title: "TRON: Legacy", year: 2010, _id: uuidv4()}
+  {title: "Project Hail Mary", year: 2026, favorite: "yes", _id: uuidv4()},
+  {title: "The Dark Knight", year: 2008, favorite: "no", _id: uuidv4()},
+  {title: "Inception", year: 2010, favorite: "no", _id: uuidv4()},
+  {title: "Interstellar", year: 2014, favorite: "yes", _id: uuidv4()},
+  {title: "TRON: Legacy", year: 2010, favorite: "yes", _id: uuidv4()}
 ];
 
-
+// return all
 movieRouter.get("/", (req, res) => {
     res.send(movies)
 })
+
+//return one
+movieRouter.get("/:movieId",(req, res) =>{
+    const movieId = req.params.movieId
+    const foundMovie = movies.find(movie => movie._id === movieId)
+    res.send(foundMovie)
+})
+
+//return certain favorites
+movieRouter.get("/search/favorite", (req, res) => {
+    const favorites = req.query.favorite
+    const filteredMovies = movies.filter(movie => movie.favorite === favorites )
+    res.send(filteredMovies)
+})
+
 
 movieRouter.post("/", (req, res) => {
     const newMovie = req.body
@@ -22,6 +37,35 @@ movieRouter.post("/", (req, res) => {
     movies.push(newMovie)    
     res.send(`Successfully added ${newMovie.title} to the database!! Here's it's ID: ${newMovie._id}`)
 })
+
+movieRouter.put('/movies/:movieId', (req, res) => {
+    const movieId = req.params.movieId
+    const updatedObject = req.body
+    const movieIndex = movies.findIndex(movie => movie._id === movieId)
+
+    if (movieIndex !== -1) {
+        const updatedMovie = Object.assign(movies[movieIndex], updatedObject)
+        res.send(updatedMovie)
+    } else {
+        res.status(404).send('Movie not found')
+    }
+})
+
+
+
+movieRouter.delete("/:movieId", (req, res) => {
+    const movieId = req.params.movieId
+    const movieIndex = movies.findIndex(movie => movie._id === movieId)
+    movies.splice(movieIndex, 1)
+
+
+})
+
+
+
+
+
+
 
 
 module.exports = movieRouter
