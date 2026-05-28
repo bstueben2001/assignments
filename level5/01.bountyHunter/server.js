b5d9f2233a06f04f14577b198bdf6bef4e74d519
Brendon Stueben <bstueben2001@gmail.com>
2026-05-28 01:35:38 -0600

    installed nodemon

--- level5/01.bountyHunter/server.js
+++ level5/01.bountyHunter/server.js
@@ -7,5 +7,5 @@ app.use(express.json())
 app.use("/bounties",require("./Routes/bountyRouter.js"))
 
 app.listen(9000, () => {
-    console.log("Server running on port 909090")
+    console.log("Server running on port 9000")
 })
