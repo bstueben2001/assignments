475727d38873ee5ee8d6ad90a5b2bd1fc83a1132
Brendon Stueben <bstueben2001@gmail.com>
2026-06-01 02:26:06 -0600

    finished sortbyProperty challenge

--- level5/03.midLevelChallenges/sortByProperty.js
+++ level5/03.midLevelChallenges/sortByProperty.js
@@ -1,5 +1,13 @@
 function sortByProperty(objects, propertyName) {
-  
+    //take "people" array
+    //use objects.sort to loop through array
+    //set arguements of sort function for a,b
+    //return a.age - b.age
+
+    const sortedByAge = objects.sort((a,b) => {
+        return a[propertyName] - b[propertyName]
+    })
+    return sortedByAge
 }
 
 const people = [
