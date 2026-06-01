2dd1d22e34dfd0f552bf92ffd7e90ce2c1d700c0
Brendon Stueben <bstueben2001@gmail.com>
2026-06-01 02:02:26 -0600

    finished extractUniqueCharacters challenge

--- level5/03.midLevelChallenges/extractUniqueCharacters.js
+++ level5/03.midLevelChallenges/extractUniqueCharacters.js
@@ -1,5 +1,19 @@
 function extractUniqueCharacters(strings) {
-  
+    //take all words in array and combine all of them into one string
+    //for loop through all chararcters
+    //conditional; if not already in array, push to uniqueChars
+    //return
+    const uniqueChars = [];
+    const combinedWord = strings.join('');
+
+    for(let i = 0; i < combinedWord.length; i++){
+        const char = combinedWord[i];
+        if(!uniqueChars.includes(char)){
+            uniqueChars.push(char)
+        };
+    };
+
+    return uniqueChars;
 }
 
 const words = ['apple', 'banana', 'cherry'];
