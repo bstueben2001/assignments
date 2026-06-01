1746a27ee4fd62c42c2fe92286a4c4d82da5a9c7
Brendon Stueben <bstueben2001@gmail.com>
2026-06-01 00:26:26 -0600

    Set up to add error handling

--- level5/01.bountyHunter/Server/server.js
+++ level5/01.bountyHunter/Server/server.js
@@ -2,9 +2,16 @@ const express = require("express")
 const app = express()
 const {v4: uuidv4} = require('uuid')
 
+//app.use is middleware
+
 app.use(express.json())
 
 app.use("/bounties",require("../Routes/bountyRouter.js"))
+
+//error handling
+app.use((err, req, res, next) => {
+    console.log(err)
+})
 
 app.listen(9000, () => {
     console.log("Server running on port 9000")
