242a3794105dcfc62c66c0d257522a1bdb552d6e
Brendon Stueben <bstueben2001@gmail.com>
2026-06-01 00:01:29 -0600

    Added database folder for better structure and understanding

--- level5/01.bountyHunter/Routes/bountyRouter.js
+++ level5/01.bountyHunter/Routes/bountyRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
-const bounties = require("../Server/bountyData");
+const bounties = require("../Database/bountyData");
 
 const bountyRouter = express.Router();
 
