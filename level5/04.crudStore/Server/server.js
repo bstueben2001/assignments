5a9f32dd333782aa24fc73cf54bb2a657842bb19
Brendon Stueben <bstueben2001@gmail.com>
2026-06-03 23:06:35 -0600

    connected to mongoDB and installed dotenv

--- level5/04.crudStore/Server/server.js
+++ level5/04.crudStore/Server/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(express.json());
 
